d4679db0ea315311026cf84f8d9e8b566ba4bf24
ackho <ackho@uw.edu>
2021-01-16 19:39:58 -0800

    last edits

--- script.js
+++ script.js
@@ -7,7 +7,7 @@
    */
   function init() {
     id("donate-btn").addEventListener("click", donate)
-    id("chat").addEventListener("click", () => window.open("chat.html"))
+    id("chat").addEventListener("click", () => window.location ="chat.html")
     id("video-call").addEventListener("click", () => window.open("call/index.html"))
   }
 
